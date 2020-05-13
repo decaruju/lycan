e976bc1b110310a203dd4200b5eceeed8011bafb
decaruju <julien.decarufel@gmail.com>
2020-05-12 21:26:02 -0400

    fix a few bugs, maybe

--- src/client/game.rs
+++ src/client/game.rs
@@ -93,8 +93,8 @@ pub fn start_game(window: &mut RenderWindow, gamestate: Arc<RwLock<ClientGamesta
                 if let TileType::Door(direction) = player_room.tile(gamestate.player_tile()).tile_type {
                     gamestate.add_room(
                         match direction {
-                            Direction::Up => (position.0, position.1 - 1),
-                            Direction::Down => (position.0, position.1 + 1),
+                            Direction::Up => (position.0, position.1 + 1),
+                            Direction::Down => (position.0, position.1 - 1),
                             Direction::Left => (position.0 - 1, position.1),
                             Direction::Right => (position.0 + 1, position.1),
                         }
