2ea3a6ee7dddc7ab1c768ace26e1398228c1eb7d
Charles <sirois.charles.@protonmail.com>
2020-05-13 00:43:14 -0400

    button more centered

--- src/client/main_menu.rs
+++ src/client/main_menu.rs
@@ -17,31 +17,35 @@ pub enum MenuChoice {
 struct MenuButton<'a> {
     title: String,
     title_text: Text<'a>,
+    position: Vector2<f32>,
     background: RectangleShape<'a>,
-    circle: CircleShape<'a>,
     size: Vector2<f32>,
 }
 
 impl<'a> MenuButton<'a> {
     fn new(title: String) -> Self {
         let size = Vector2::from((300., 100.));
+        let position = Vector2::from((0., 0.));
         let mut background = RectangleShape::with_size(size);
-        background.set_origin((size.x / 2., size.y / 2.));
-
-        let mut circle = the_ball();
-        circle.set_position((150., 50.));
+        let center = (size.x / 2., size.y / 2.);
+        background.set_origin(center);
         // TODO only work if run from client folder
         let mut title_text = Text::default();
         title_text.set_string(title.as_str());
-        title_text.set_position((50., 20.));
         title_text.set_fill_color(Color::RED);
+        title_text.set_origin(center);
         Self {
             title: title,
             title_text: title_text,
+            position: position,
             background: background,
-            circle: circle,
             size: size,
         }
+    }
+
+    fn set_position(&mut self, position: (f32, f32)) {
+        self.background.set_position(position);
+        self.title_text.set_position(position);
     }
 }
 
@@ -51,9 +55,7 @@ impl<'a> Drawable for MenuButton<'a> {
         render_target: &mut dyn RenderTarget,
         _: RenderStates<'texture, 'shader, 'shader_texture>,
     ) {
-        render_target.size();
         render_target.draw(&self.background);
-        render_target.draw(&self.circle);
         render_target.draw(&self.title_text);
     }
 }
@@ -87,9 +89,16 @@ pub fn main_menu(setting: &mut Settings, window: &mut RenderWindow) -> MenuChoic
         if mouse::Button::Left.is_pressed() {}
 
         if Key::Space.is_pressed() {}
+
+        let size = window.size();
+        let center_window = Vector2::from((size.x as i32 / 2, size.y as i32 / 2));
+        let center = window.map_pixel_to_coords(center_window, &menu_view);
+        menu_view.set_center(center);
         window.clear(Color::BLUE);
         window.set_view(&menu_view);
-        startgame_button.texture.set_position((100., 100.));
+
+        startgame_button.set_position((center.x, center.y));
+
         window.draw(&startgame_button);
         window.draw(&theball);
         window.display();
