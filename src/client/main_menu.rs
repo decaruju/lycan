702a5a04374929fcac94d6e2fa5751e8f44873e5
Charles <sirois.charles.@protonmail.com>
2020-05-12 22:07:25 -0400

    font fun

--- src/client/main_menu.rs
+++ src/client/main_menu.rs
@@ -1,7 +1,7 @@
 use sfml::{
     graphics::{
-        CircleShape, Color, Drawable, Rect, RectangleShape, RenderStates, RenderTarget,
-        RenderWindow, Shape, Transformable, View,
+        CircleShape, Color, Drawable, Font, Rect, RectangleShape, RenderStates, RenderTarget,
+        RenderWindow, Shape, Text, Transformable, View,
     },
     system::Vector2,
     window::{mouse, ContextSettings, Event, Key, Style},
@@ -16,19 +16,31 @@ pub enum MenuChoice {
 
 struct MenuButton<'a> {
     title: String,
-    texture: RectangleShape<'a>,
+    title_text: Text<'a>,
+    background: RectangleShape<'a>,
     circle: CircleShape<'a>,
+    size: Vector2<f32>,
 }
 
 impl<'a> MenuButton<'a> {
     fn new(title: String) -> Self {
         let size = Vector2::from((300., 100.));
-        let mut texture = RectangleShape::with_size(size);
-        texture.set_origin((size.x / 2., size.y / 2.));
+        let mut background = RectangleShape::with_size(size);
+        background.set_origin((size.x / 2., size.y / 2.));
+
+        let mut circle = the_ball();
+        circle.set_position((150., 50.));
+        // TODO only work if run from client folder
+        let mut title_text = Text::default();
+        title_text.set_string(title.as_str());
+        title_text.set_position((50., 20.));
+        title_text.set_fill_color(Color::RED);
         Self {
             title: title,
-            texture: texture,
-            circle: the_ball(),
+            title_text: title_text,
+            background: background,
+            circle: circle,
+            size: size,
         }
     }
 }
@@ -39,14 +51,18 @@ impl<'a> Drawable for MenuButton<'a> {
         render_target: &mut dyn RenderTarget,
         _: RenderStates<'texture, 'shader, 'shader_texture>,
     ) {
-        render_target.draw(&self.texture);
+        render_target.size();
+        render_target.draw(&self.background);
         render_target.draw(&self.circle);
+        render_target.draw(&self.title_text);
     }
 }
 
 pub fn main_menu(setting: &mut Settings, window: &mut RenderWindow) -> MenuChoice {
     let mut theball = the_ball();
+    let font = Font::from_file("resources/VCR_OSD_MONO_1.001.ttf").unwrap();
     let mut startgame_button = MenuButton::new(String::from("start the game"));
+    startgame_button.title_text.set_font(&font);
     let mut menu_view = View::new(
         Vector2::from((0., 0.)),
         Vector2::from((window.size().x as f32, window.size().y as f32)),
@@ -68,11 +84,9 @@ pub fn main_menu(setting: &mut Settings, window: &mut RenderWindow) -> MenuChoic
             return MenuChoice::StartGame;
         }
 
-        if mouse::Button::Left.is_pressed() {
-        }
+        if mouse::Button::Left.is_pressed() {}
 
-        if Key::Space.is_pressed() {
-        }
+        if Key::Space.is_pressed() {}
         window.clear(Color::BLUE);
         window.set_view(&menu_view);
         startgame_button.texture.set_position((100., 100.));
